58c757460f895f0865bae552b33677bd3cbb9ce3
etherealeon <mrr696@yandex.ru>
2020-10-27 15:11:18 +0300

    Create geocode.py

---
+++ geocode.py
@@ -0,0 +1,34 @@
+import requests
+from loll import Configs
+
+
+def getCoords(city):
+    PARAMS = {
+        "apikey": Configs.yan,
+        "format": "json",
+        "lang": "ru_RU",
+        "kind": "locality",
+        "geocode": city
+    }
+    try:
+        r = requests.get(url="https://geocode-maps.yandex.ru/1.x/", params=PARAMS)
+        json_data = r.json()
+        results = int(json_data["response"]["GeoObjectCollection"]["metaDataProperty"]["GeocoderResponseMetaData"]
+                      ["results"])
+        found = int(
+            json_data["response"]["GeoObjectCollection"]["metaDataProperty"]["GeocoderResponseMetaData"]["found"])
+        num_places = min(results, found)
+        address = []
+        for i in range(0, num_places):
+            d = {json_data["response"]["GeoObjectCollection"]["featureMember"][i]["GeoObject"]
+                 ["metaDataProperty"]["GeocoderMetaData"]["AddressDetails"]["Country"]["AddressLine"]:
+                     json_data["response"]["GeoObjectCollection"]["featureMember"][i]["GeoObject"]["Point"]["pos"]}
+            address.append(d)
+            """address_str.append(json_data["response"]["GeoObjectCollection"]["featureMember"][i]["GeoObject"]
+                               ["metaDataProperty"]["GeocoderMetaData"]["AddressDetails"]["Country"]["AddressLine"])
+            address_loc.append(json_data["response"]["GeoObjectCollection"]["featureMember"][i]["GeoObject"]["Point"]
+                               ["pos"])"""
+        return address
+    # make this exc NOT 2 broad but how.........
+    except Exception as e:
+        return "error"
